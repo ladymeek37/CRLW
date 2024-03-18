c13548ac051f230ac2cf605a228320d39c2f7496
Lady Meek <ladyjmeek@gmail.com>
2024-03-18 10:59:11 -0700

    add property map to rock house

--- frontend/src/pages/RockHouse/RockHouse.js
+++ frontend/src/pages/RockHouse/RockHouse.js
@@ -33,6 +33,10 @@ const RockHousePage = () => {
                         >
                     </iframe>  
                 </div>
+                            <h2> Location Map </h2>
+                <div className="ppchild">
+                    <iframe className= "googlemap" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4255.194275109196!2d-83.72352092497776!3d9.303431890769257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOcKwMTgnMTIuNCJOIDgzwrA0MycxNS40Ilc!5e1!3m2!1sen!2sus!4v1710784307213!5m2!1sen!2sus" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             <h2>  Surverys & Maps  </h2>            
                 <div className="ppchild">
                     <ImageGallery items={rockhousemaps}
