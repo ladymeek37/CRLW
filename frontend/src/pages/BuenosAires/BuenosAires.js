a729fbf3dcf57225040d4cffaa9419935523f47e
Lady Meek <ladyjmeek@gmail.com>
2024-08-17 15:23:37 -0700

    updated buenos aires photos

--- frontend/src/pages/BuenosAires/BuenosAires.js
+++ frontend/src/pages/BuenosAires/BuenosAires.js
@@ -4,80 +4,72 @@ import "react-image-gallery/styles/css/image-gallery.css"
 import "../PropertyPage.css"
 
 //Photo Imports
-import BlackHorseBA from "./BestofBuenosAires/BlackHorseBA.JPG"
-import BrownHorseBA from "./BestofBuenosAires/BrownHorseBA.JPG"
-import CharliBA from "./BestofBuenosAires/CharliBA.JPG"
-import Cows1BA from "./BestofBuenosAires/Cows1BA.JPG"
-import Cows2BA from "./BestofBuenosAires/Cows2BA.JPG"
-import Cows3BA from "./BestofBuenosAires/Cows3BA.JPG"
-import Cows4BA from "./BestofBuenosAires/Cows4BA.JPG"
-import CranesBA from "./BestofBuenosAires/CranesBA.JPG"
-import PapaCharBA from "./BestofBuenosAires/PapaCharBA.JPG"
-import PiggyBA from "./BestofBuenosAires/PiggyBA.JPG"
-import PoopyPiggyBA from "./BestofBuenosAires/PoopyPiggyBA.JPG"
+import CowsMainBA from "./BestofBuenosAires/CowsMainBA.JPG"
+import DonkeyPOVBA from "./BestofBuenosAires/DonkeyPOVBA.jpeg"
+import HouseBA from "./BestofBuenosAires/HouseBA.JPG"
+import LivestockPenBA from "./BestofBuenosAires/LivestockPenBA.jpeg"
+import LivestockPenBA2 from "./BestofBuenosAires/LivestockPenBA2.jpeg"
+import LivestockPenBA3 from "./BestofBuenosAires/LivestockPenBA3.JPG"
+import PastureBA from "./BestofBuenosAires/PastureBA.JPG"
+import RiverBA from "./BestofBuenosAires/RiverBA.jpeg"
+import RiverBA2 from "./BestofBuenosAires/RiverBA2.JPG"
+import RiverBA3 from "./BestofBuenosAires/RiverBA3.JPG"
 import RiverCharBA from "./BestofBuenosAires/RiverCharBA.JPG"
-import RiverChar2BA from "./BestofBuenosAires/RiverChar2BA.JPG" 
-import WaterBuffalo1BA from "./BestofBuenosAires/WaterBuffalo1BA.JPG"
-import WaterBuffalo2BA from "./BestofBuenosAires/WaterBuffalo2BA.JPG"
 import WaterBuffalo3BA from "./BestofBuenosAires/WaterBuffalo3BA.JPG"
+
 
 
 const BuenosAiresPage = () => {
 
     const images = [
         {
-            original: Cows2BA,
-            thumbnail: Cows2BA, 
+            original: CowsMainBA,
+            thumbnail: CowsMainBA, 
         },
         {
-            original: Cows1BA,
-            thumbnail: Cows1BA, 
+            original: DonkeyPOVBA,
+            thumbnail: DonkeyPOVBA, 
         },
         {
-            original: Cows3BA,
-            thumbnail: Cows3BA, 
+            original: HouseBA,
+            thumbnail: HouseBA, 
         },
         {
-            original: Cows4BA,
-            thumbnail: Cows4BA, 
+            original: LivestockPenBA,
+            thumbnail: LivestockPenBA, 
         },
         {
-            original: WaterBuffalo1BA,
-            thumbnail: WaterBuffalo1BA, 
+            original: LivestockPenBA2,
+            thumbnail: LivestockPenBA2, 
         },
         {
-            original: WaterBuffalo2BA,
-            thumbnail: WaterBuffalo2BA, 
+            original: LivestockPenBA3,
+            thumbnail: LivestockPenBA3, 
         },
         {
-            original: WaterBuffalo3BA,
-            thumbnail: WaterBuffalo3BA, 
+            original: PastureBA,
+            thumbnail: PastureBA, 
         },
         {
-            original: RiverChar2BA,
-            thumbnail: RiverChar2BA, 
+            original: RiverBA,
+            thumbnail: RiverBA, 
+        },
+        {
+            original: RiverBA2,
+            thumbnail: RiverBA2, 
+        },
+        {
+            original: RiverBA3,
+            thumbnail: RiverBA3, 
         },
         {
             original: RiverCharBA,
             thumbnail: RiverCharBA, 
         },
         {
-            original: BlackHorseBA,
-            thumbnail: BlackHorseBA, 
+            original: WaterBuffalo3BA,
+            thumbnail: WaterBuffalo3BA,
         },
-        {
-            original: BrownHorseBA,
-            thumbnail: BrownHorseBA, 
-        },
-        {
-            original: CranesBA,
-            thumbnail: CranesBA, 
-        },
-        {
-            original: CharliBA,
-            thumbnail: CharliBA, 
-        },
-        
     ]
     return ( 
         <div className = "ppcontainer">
